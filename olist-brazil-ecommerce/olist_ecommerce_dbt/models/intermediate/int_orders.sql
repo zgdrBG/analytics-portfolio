4cd6a6bfc759cca03af61dbe8532ec6071c0dda4
zgdrBG <zdravkogaydarov@gmail.com>
2026-09-05 09:45:27 +0300

    fix logic in delivery calcs

--- olist-brazil-ecommerce/olist_ecommerce_dbt/models/intermediate/int_orders.sql
+++ olist-brazil-ecommerce/olist_ecommerce_dbt/models/intermediate/int_orders.sql
@@ -76,23 +76,25 @@ orders_and_payments AS (
         ) AS days_to_deliver_customer,
         DATEDIFF(
             DAY,
-            staging_orders.order_delivered_customer_date,
+            order_purchase_date,
             staging_orders.order_estimated_delivery_date
         ) AS estimated_days_to_deliver_customer,
         (
-            estimated_days_to_deliver_customer - days_to_deliver_customer
+            days_to_deliver_customer - estimated_days_to_deliver_customer
         ) AS estimation_delivery_days_diff,
-        COALESCE(estimation_delivery_days_diff >= 0, TRUE) AS is_delivery_late,
+        estimation_delivery_days_diff > 0 AS is_delivery_late,
         CASE
             WHEN is_delivery_late
-                AND estimation_delivery_days_diff >= -3
-                    THEN 'Minor'
+                AND estimation_delivery_days_diff > 7
+                    THEN 'Severe'
             WHEN is_delivery_late
-                AND estimation_delivery_days_diff >= -7
+                AND estimation_delivery_days_diff > 3
                     THEN 'Moderate'
             WHEN is_delivery_late
-                THEN 'Severe'
-            ELSE 'On-time'
+                THEN 'Minor'
+            WHEN NOT is_delivery_late
+                THEN 'On-time'
+            ELSE is_delivery_late
         END AS delivery_delay_type
     FROM staging_orders
     LEFT JOIN order_payments_agg
